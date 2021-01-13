8cbc4255e7516b7e282ad0041f7f866f11e60cb5
HunkiKim <gnsrl76@naver.com>
2021-01-13 21:14:23 +0900

    입출력 업데이트

--- 11022.py
+++ 11022.py
@@ -1,13 +1,16 @@
 import string
 n = int(input())
 L = list()
-
+CL = list()
+DL = list()
 
 for x in range(n):
     A = input()
     B = A.split(',')
     C = int(A[0])
+    CL.append(int(A[0]))
     D = int(A[2])
+    DL.append(int(A[2]))
     L.append(C + D)
 
 num = 1
@@ -15,6 +18,6 @@ num = 1
 for x in L:
     s = "Case #"
     numb = str(num)
+    new = s + numb + ": " + str(CL[num-1]) + " + " + str(DL[num-1]) + " = " + str(x)
     num += 1
-    new = s + numb + ": " + str(x)
     print(new)
---
+++ 11718.py
@@ -0,0 +1,29 @@
+# 최대 100줄 빈줄금지 공백끝금지
+# 소문자 대문자 공백 숫자만
+L = list()
+num = 0
+while 1:
+    try:
+        A = input()
+        if A == '':
+            print('error')
+            exit()
+        elif A[-1] == ' ':
+            print('error')
+            exit()
+        elif len(A) > 100:
+            print('error')
+            exit()
+        elif num == 100:
+            break
+        else:
+            num += 1
+            L.append(A)
+    except EOFError:
+        break
+for x in L:
+    print(x)
+
+
+
+
